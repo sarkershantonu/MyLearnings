39e6a2bcf1b4ee05c04a8c624fc6b9d345aab737
shantonu sarker <shantonu_oxford@yahoo.com>
2018-11-02 23:12:23 -0400

    package updated

--- tutoring-parent/content-api/src/main/java/org/tutoring/api/web/ui/HomeController.java
+++ tutoring-parent/content-api/src/main/java/org/tutoring/api/web/ui/HomeController.java
@@ -1,16 +1,17 @@
-package org.tutoring.api.web;
+package org.tutoring.api.web.ui;
 
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.tutoring.api.web.BaseController;
 
 /**
  * Created by SSarker on 10/27/2018.
  */
 @Controller
-public class HomeController extends BaseController{
+public class HomeController extends BaseController {
     @GetMapping("/home")
     public String home(Model model) {
         return "forward:/index.html";
