f4a0b967975755a71fbf83e75ffe991da996fdca
shantonu sarker <shantonu_oxford@yahoo.com>
2018-11-03 02:11:03 -0400

    e

--- tutoring-parent/content-api/src/main/java/org/tutoring/api/emums/FileTypes.java
+++ tutoring-parent/content-api/src/main/java/org/tutoring/api/emums/FileTypes.java
@@ -1,4 +1,18 @@
 package org.tutoring.api.emums;
 
 public enum FileTypes {
+
+    TXT("txt"),
+    VIDEO("video"),
+    IMAGE("image"),
+    AUDIO("audio"),
+    DOC("doc"),
+    DOCX("docx"),
+    PDF("pdf"),
+    HTML("html");
+    private String id;
+
+    FileTypes(String id) {
+        this.id = id;
+    }
 }
--- tutoring-parent/content-api/src/main/java/org/tutoring/api/model/content/FileInfo.java
+++ tutoring-parent/content-api/src/main/java/org/tutoring/api/model/content/FileInfo.java
@@ -1,4 +1,19 @@
 package org.tutoring.api.model.content;
 
+import org.tutoring.api.emums.FileTypes;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import java.io.File;
+
+@Entity
 public class FileInfo {
+    private String name;
+    private String description;
+    private String extention;
+    @Enumerated(EnumType.STRING)
+    private FileTypes fileType;
+    private Long size;
+    private File file;
 }
