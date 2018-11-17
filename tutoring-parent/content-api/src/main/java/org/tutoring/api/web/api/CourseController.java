6423ee9d1af30ac50138df6b2821cc082cb3185e
shantonu sarker <shantonu_oxford@yahoo.com>
2018-11-17 09:59:48 -0500

    version update

--- tutoring-parent/content-api/src/main/java/org/tutoring/api/web/api/CourseController.java
+++ tutoring-parent/content-api/src/main/java/org/tutoring/api/web/api/CourseController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.tutoring.api.model.Course;
